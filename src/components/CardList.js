159e74902859ce35826c5690ef6b5f7229ce2e7e
xipholena <you@example.com>
2021-05-06 17:41:53 +0300

    no errors option

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-    if(true) {
+    /*if(true) {
         throw new Error('Nooooo!');
-    }
+    }*/
     return (
         <ul>
             <li>
